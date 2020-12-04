a503cc978b04582fef6a5a3259db1393d1f87354
clesiorods <clesiorodrigues96@gmail.com>
2020-12-04 09:09:32 -0300

    Projeto finalizado

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Header from "./components/Header";
+import Routes from "./routes";
+import './styles.css';
+
+const App = () => {
+  return(
+    <div className="App">
+      <Header/>
+      <Routes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+    return(
+        <header id="main-header" > JSHunt </header>
+    );
+};
+
+export default Header; 
